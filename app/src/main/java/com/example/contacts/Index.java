67c47734ef5ec1f2bb6dc09a12f60fbf6d6c4f01
taehyek0314 <taehyek0314@gmail.com>
2019-04-15 18:14:08 +0900

    2019-04-15 18:14

---
+++ app/src/main/java/com/example/contacts/Index.java
@@ -0,0 +1,14 @@
+package com.example.contacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+    }
+}
